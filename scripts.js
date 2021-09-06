b87e4f62fd2dbe5514249264d44eae99f90c54d0
Antonio Barrera <jeantonio.barrera@gmail.com>
2021-09-06 16:24:37 -0600

    output place and mock API changed

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var inputText = document.querySelector("#input-text");
 var outputText = document.querySelector("#output-text");
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(text){
     return serverURL + "?text=" + text;
@@ -16,7 +17,10 @@ function clickEvent(){
     var textinput = inputText.value;
     fetch(getTranslationURL(textinput))
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputText.innerText = translatedText;
+    })
     .catch(errorHandler)
 }
 
