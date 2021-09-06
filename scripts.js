9ed3d63e1b39af8a8f28b647399b92ebea45afc7
Antonio Barrera <jeantonio.barrera@gmail.com>
2021-09-06 16:23:09 -0600

    added error handler function

--- scripts.js
+++ scripts.js
@@ -1,9 +1,23 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var inputText = document.querySelector("#input-text");
+var outputText = document.querySelector("#output-text");
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getTranslationURL(text){
+    return serverURL + "?text=" + text;
+}
+
+function errorHandler(error){
+    console.log("error ocurred", error);
+    alert("Something wrong with the server");
+}
 
 function clickEvent(){
-    console.log(inputText.value);
-
+    var textinput = inputText.value;
+    fetch(getTranslationURL(textinput))
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(errorHandler)
 }
 
 btnTranslate.addEventListener("click", clickEvent);
